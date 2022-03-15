e528bc8bfbb746ce86bf93c6bf2881bc0a617923
okabekaito <kaito19991204@gmail.com>
2022-03-15 11:08:35 +0900

    タイトル変更

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -5,13 +5,9 @@ import { memo, VFC } from "react";
 export const Home: VFC = memo(() => {
     return (
         <>
-        <Box bg="#FED7D7"h="250px"  borderRadius="10px" shadow="md" m={10} p={10}>
-        <Text fontSize='6xl' textAlign="center">Home</Text>
-        <Text fontSize='lg'textAlign="center">ここは作成物や学んできたことを紹介するサイトです。ReactとTypeScriptを使って作成しました。</Text>
-        </Box>
         <Box bg="#FEEBC8" borderRadius="10px" shadow="md" m={10} p={10}>
         <Text fontSize='6xl'textAlign="center">自己紹介</Text>
-        <Text fontSize='lg'textAlign="center">大学4年生経営学部の岡部海斗です。</Text>
+        <Text fontSize='lg'textAlign="center">岡部海斗です。現在大学4年生で経営学を学んでいます。</Text>
         <Text fontSize='lg'textAlign="center">現在はプログラミングを勉強するために大学を休学していて、来年卒業します。</Text>
         </Box>
         <Box bg="#F0FFF4" borderRadius="10px" shadow="md" m={10} p={10}>
@@ -22,6 +18,7 @@ export const Home: VFC = memo(() => {
             <Text textAlign="center">React</Text>
             <Text textAlign="center">Ruby(Ruby on Rails)</Text>
             <Text textAlign="center">php(Laravel)</Text>
+            <Text textAlign="center">Java</Text>
             <Text textAlign="center">データーベース(SQL)</Text>
             <Text textAlign="center">ネットワーク</Text>
             <Text textAlign="center">web技術</Text>
